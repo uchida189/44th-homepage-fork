1bcdce299a6b9ad197778a23e57a4c3ff5da8890
uchida189 <perukipoo@icloud.com>
2025-05-28 13:55:26 +0900

    [fix]export const runtime = 'edge'のコードを先頭に移動

--- app/pages/event/plan/[id]/page.tsx
+++ app/pages/event/plan/[id]/page.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 export const runtime = 'edge';
+
 export default function PlanDetailPage() {
   return (
     <div>
--- app/pages/sale/[id]/page.tsx
+++ app/pages/sale/[id]/page.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 export const runtime = 'edge';
 export default function SaleDetailPage() {
   return (
